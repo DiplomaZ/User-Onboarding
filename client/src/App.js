7f1d8f3743717fca7abbacc9e055e934c922fdd7
whoIsJosh_ <joshua.c.luscombe@gmail.com>
2020-02-21 14:35:33 -0800

    mvp push

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,32 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import logo from "./logo.svg";
 import "./App.css";
-import OnboardingForm from "./components/OnboardingForm";
+import FormikOnboardingForm from "./components/OnboardingForm";
+import FormikLoginForm from "./components/FormikLoginForm";
 function App() {
+  const testData = [
+    {
+      username: "BILL"
+    }
+  ];
+  const [users, setUsers] = useState(testData);
+  const addUser = newUser => {
+    setUsers([...users, newUser]);
+  };
+
+  useEffect(() => {
+    console.log(users);
+  }, [users]);
+
+  console.log(users);
   return (
     <div className="App">
-      <OnboardingForm />
+      <FormikOnboardingForm addUser={addUser} />
+      <h1>
+        {users.map(userObj => {
+          return <h1>{userObj.username}</h1>;
+        })}
+      </h1>
     </div>
   );
 }
